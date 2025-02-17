df9f9406b684795a76ec1c3d2f1b410421e6592e
tapoka <taharboussak@gmail.com>
2025-02-17 10:22:36 +0100

    final

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,25 +1,14 @@
 import * as sdk from "node-appwrite";
 
-export const PROJECT_ID = "675feaf100148017bb12";
-export const API_KEY =
-  "standard_f75120e6cb733166a23a9987f1d2cdb1c65d1af5ec413d74d6a98b2a834b2ab0521459367a709cbad41ca20405d153834cfcc26bf203f5292d96efad28a96e1b7767ab8ceac4d9a8d9c48f5d4738f5ede76551d81cadfc48cfc7a8d7b8e864d06d12041f5bcc7883d08efb3412f070b9a39a8684d0df771729b286082613395d";
-export const DATABASE_ID = "675febe900343f0bf015";
-export const PATIENT_COLLECTION_ID = "675fec21002eceebb628";
-export const DOCTOR_COLLECTION_ID = "675fec61002828e22ecd";
-export const APPOINTEMENT_COLLECTION_ID = "675feca2000d3fee8f59";
-export const BUCKET_ID = "675fed170028303ff32b";
-export const ENDPOINT = "https://cloud.appwrite.io/v1";
-
-// export const {
-//   PROJECT_ID,
-//   API_KEY,
-//   DATABASE_ID,
-//   PATIENT_COLLECTION_ID,
-//   DOCTOR_COLLECTION_ID,
-//   APPOINTEMENT_COLLECTION_ID,
-//   NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
-//   NEXT_PUBLIC_ENDPOINT: ENDPOINT,
-// } = process.env;
+export const PROJECT_ID = process.env.PROJECT_ID;
+export const API_KEY = process.env.API_KEY;
+export const DATABASE_ID = process.env.DATABASE_ID?.toString();
+export const PATIENT_COLLECTION_ID = process.env.PATIENT_COLLECTION_ID;
+export const DOCTOR_COLLECTION_ID = process.env.DOCTOR_COLLECTION_ID;
+export const APPOINTEMENT_COLLECTION_ID =
+  process.env.APPOINTEMENT_COLLECTION_ID;
+export const BUCKET_ID = process.env.NEXT_PUBLIC_BUCKET_ID;
+export const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
 
 const client = new sdk.Client();
 
